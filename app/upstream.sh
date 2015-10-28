15772a9cd0ed3c235406c511042e4cd50fdf25e8
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-28 01:05:08 +0000

    Add newfile app/upstream.sh

---
+++ app/upstream.sh
@@ -0,0 +1,65 @@
+git checkout ArcanaMagus-debugger
+git push --set-upstream origin ArcanaMagus-debugger
+
+git checkout Readme-1
+git push --set-upstream origin Readme-1
+
+git checkout api
+git push --set-upstream origin api
+
+git checkout array
+git push --set-upstream origin array
+
+git checkout binary
+git push --set-upstream origin binary
+
+git checkout build
+push --set-upstream origin build
+
+git checkout config
+git push --set-upstream origin config
+
+git checkout debug
+git push --set-upstream origin debug
+
+git checkout debugger
+git push --set-upstream origin debugger
+
+git checkout index
+git push --set-upstream origin index
+
+git checkout init
+git push --set-upstream origin init
+
+git checkout master
+git push --set-upstream origin master
+
+git checkout origin
+git --set-upstream origin origin
+
+git checkout package
+git --set-upstream origin package
+
+git checkout param
+git --set-upstream origin param
+
+git checkout project
+git --set-upstream origin project
+
+git checkout readme
+git push --set-upstream origin readme
+
+git checkout userAuth
+git push --set-upstream origin userAuth
+
+git checkout userAuth.api
+git push --set-upstream origin userAuth.api
+
+git checkout userAuth.app
+git push --set-upstream origin userAuth.app
+
+git checkout userAuth.cpl
+git push --set-upstream origin uerAuth.apl
+
+git checkout xamarin-build
+git push --set-upstream origin xamarin-build 
