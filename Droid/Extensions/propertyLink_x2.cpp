63a988c68bad099ea4dd86f904886154ce07e988
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-11-20 02:18:04 +0000

    Add new file Droid/Extensions/propertyLink_x2.cpp

--- Droid/Extensions/propertyLink_x2.cpp
+++ Droid/Extensions/propertyLink_x2.cpp
@@ -1,36 +1,1 @@
-/*
- * propertyLink_x2.cpp
- *
- *  Created on: Nov 6, 2015
- *      Author: RonThomas
- */
-#include<iostream>
-#include<stdio>
-#include<stdlib>
-using namespoace userAuth.cpl.Droid.Extensions
-
-		// Declaring Nodes
-		struct node
-		{
-	int property;
-	scruct index $Path;
-	struct node $Property;
-	struct node $Element;
-		}
-
-class multi_element
-{
-private:
-			void create_element(bool key);
-			void create_start(int key);
-			void add_start(int key);
-			void create_path(bool value, int location);
-			void display_element(bool key);
-			void modify_elemnt(int key);
-			void construct_node(int key);
-			void after_display(int value);
-
-		};
-
-
-
+/* * propertyLink_x2.cpp * *  Created on: Nov 6, 2015 *      Author: RonThomas */#include<iostream>#include<stdio>#include<stdlib>using namespoace userAuth.cpl.Droid.Extensions		// Declaring Nodes		struct node		{	int property;	scruct index $Path;	struct node $Property;	struct node $Element;		}class multi_element{private:			void create_element(bool key);			void create_start(int key);			void add_start(int key);			void create_path(bool value, int location);			void display_element(bool key);			void modify_elemnt(int key);			void construct_node(int key);			void after_display(int value);		};
