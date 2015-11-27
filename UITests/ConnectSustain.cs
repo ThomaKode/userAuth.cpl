5c10526f39257ea00c981445de636dd115f885bc
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-11-27 17:18:48 -0400

    Modify file UITests/ConnectSustain.cs

---
+++ UITests/ConnectSustain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// A class to inhibit the termination of a connection 
+/// already established.
+/// </summary>
+
+namespace userAuth.cpl.UITests.Connect
+{
+    public class ConnectSustain
+    {
+        public static void Connect()
+        { }
+            interface wifi { System.builtin.function req, res; }
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    } namespace userAUuth.cpl.Sustain
+{
+
+}
+
